48315013da7104d3685b975bb683f046abf8dff6
nthndy <nthndy@gmail.com>
2021-03-19 17:34:30 +0000

    Added automated renderer for quick plotting

--- cell_radial_analysis/render.py
+++ cell_radial_analysis/render.py
@@ -23,10 +23,79 @@ import matplotlib.pyplot as plt
 import tools
 import numpy as np
 import os
+from pathlib import Path
+import time
+
+def auto_plot_cumulative(input_2d_hist, input_type, N, num_bins, radius, t_range, focal_cell, focal_event, subject_cell, subject_event, save_parent_dir, SI):
+
+        xlocs, xlabels, ylocs, ylabels = tools.kymo_labels(num_bins, 1, radius, t_range, SI)
+
+        ## formatting cell and event names
+        focal_event_name = 'apoptoses' if focal_event == 'APOPTOSIS' else 'divisions'
+        focal_cell_name = 'wild-type' if focal_cell == 'WT' else 'Scribble'
+        subj_event_name = 'apoptoses' if focal_event == 'APOPTOSIS' else 'divisions'
+        subj_cell_name = 'wild-type' if subject_cell == 'WT' else 'Scribble'
+
+        ## output save path formatting
+        save_dir_name = '{}_{}_{}_{}'.format(focal_cell.lower(), focal_event.lower()[0:3] if focal_event == 'DIVISION' else focal_event.lower()[0:4], subject_cell.lower(), subject_event.lower()[0:3] if subject_event == 'DIVISION' else subject_event.lower()[0:4])
+        save_dir = '{},{}/{}'.format(radius,num_bins,save_dir_name)
+        save_path = os.path.join(save_parent_dir,save_dir)
+        Path(save_path).mkdir(parents=True, exist_ok=True)
+
+        ## title formatting
+        if input_type == 'N_cells':
+            title = 'Spatiotemporal dist. of {} cells \n around {} {} (N={})'.format(subj_cell_name,focal_cell_name, focal_event_name, N)
+            cb_label = 'Number of {} cell apperances'.format(subj_cell_name)
+        if input_type == 'N_events':
+            title = 'Spatiotemporal dist. of {} {} \n around {} {} (N={})'.format(subj_cell_name, subj_event_name, focal_cell_name, focal_event_name, N)
+            cb_label = 'Number of {} {}'.format(subj_cell_name, subj_event_name)
+        if input_type == 'P_events':
+            title = 'Spatiotemporal dist. of probability of {} {} \n around {} {} (N={})'.format(subj_cell_name, subj_event_name, focal_cell_name, focal_event_name, N)
+            cb_label = 'Probability of {} {}'.format(subj_cell_name, subj_event_name)
+
+        ## label unit formatting
+        if SI == True:
+            time_unit = '(Hours)'
+            distance_unit = '(Micrometers)'
+        else:
+            time_unit = '(Frames)'
+            distance_unit = '(Pixels)'
+
+        ## plotting
+        plt.xticks(xlocs, xlabels, rotation = 'vertical')
+        plt.yticks(ylocs, ylabels)
+        plt.xlabel("Time since apoptosis "+ time_unit)
+        plt.ylabel("Distance from apoptosis "+ distance_unit)
+        plt.title(title)
+
+        final_hist = np.flipud(input_2d_hist)  ## flip for desired graph orientation
+        plt.imshow(final_hist)
+        plt.colorbar(label = cb_label)
+
+        ## apop location marker
+        plt.scatter(num_bins/2-0.5, num_bins-0.75, s=20, c='white', marker='v')
+        plt.text(num_bins+0.5, num_bins+3, 'Apoptosis location \nshown by inverted \nwhite triangle')
+
+        fn = os.path.join(save_path,title+'.pdf')
+
+        ## failsafe overwriting block
+        if os.path.exists(fn):
+            print("Filename", fn, "already exists, saving as updated copy")
+            fn = fn.replace('.pdf', ' (updated {}).pdf'.format(time.strftime("%Y%m%d-%H%M%S")))
+
+
+        ## save out?
+        if save_parent_dir == '':
+            return plt.imshow(final_hist)
+        else:
+            plt.savefig(fn, dpi = 300, bbox_inches = 'tight')
+            print("Plot saved at ", fn)
+            return plt.imshow(final_hist)
+
 
 def plot_cumulative(input_2d_hist, num_bins, radius, t_range, title, label, cb_label, save_path, SI):
 
-        xlocs, xlabels, ylocs, ylabels = tools.kymo_labels(num_bins, 1, radius, t_range, SI= True)
+        xlocs, xlabels, ylocs, ylabels = tools.kymo_labels(num_bins, 1, radius, t_range, SI)
 
         if SI == True:
             time_unit = '(Hours)'
