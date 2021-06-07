39b0915efc247a2012382bfebd73192c7ea9c7ce
nthndy <nthndy@gmail.com>
2021-06-07 17:35:54 +0100

    Important change to render.py as well as more statistical tests

--- radial_analysis/render.py
+++ radial_analysis/render.py
@@ -36,6 +36,8 @@ from tqdm.notebook import tnrange, tqdm
 import matplotlib.pyplot as plt
 import tools
 from datetime import datetime
+import matplotlib.font_manager
+
 
 """
 Graph rendering below
@@ -43,7 +45,7 @@ Graph rendering below
 This section takes the final output of my radial analysis and renders the relevant graphs and labels
 """
 
-def auto_plot_cumulative(input_2d_hist, input_type, N, num_bins, radius, t_range, focal_cell, focal_event, subject_cell, subject_event, save_parent_dir, cbar_lim, SI):
+def auto_plot_cumulative_defaulttext(input_2d_hist, input_type, N, num_bins, radius, t_range, focal_cell, focal_event, subject_cell, subject_event, save_parent_dir, cbar_lim, include_apop_bin, SI):
 
         xlocs, xlabels, ylocs, ylabels = kymo_labels(num_bins, 1, radius, t_range, SI)
 
@@ -88,20 +90,30 @@ def auto_plot_cumulative(input_2d_hist, input_type, N, num_bins, radius, t_range
         plt.yticks(ylocs, ylabels)
         plt.xlabel("Time since apoptosis "+ time_unit)
         plt.ylabel("Distance from apoptosis "+ distance_unit)
-        plt.title(title)
+        plt.title(title+'\n', fontweight='bold')
 
-        final_hist = np.flipud(input_2d_hist)  ## flip for desired graph orientation
+        ## if include_apop_bin is true then the spatial bin containing the apoptotic cell (ie the central spatial bin of the radial scan) will be show in the graph, if false then it is cropped which ends up with a plot showing only the relevant local env not the site of apop (better imo)
+        if include_apop_bin == True:
+            final_hist = np.flipud(input_2d_hist)  ## flip for desired graph orientation
+        else:
+            final_hist = np.flipud(input_2d_hist[1:-1,:])
+
         plt.imshow(final_hist)
 
         if cbar_lim == '':
+            #plt.clim(vmin=cbar_lim[0], vmax=cbar_lim[1])
             plt.colorbar(label = cb_label)
         else:
             plt.clim(vmin=cbar_lim[0], vmax=cbar_lim[1])
             plt.colorbar(label = cb_label)
 
         ## apop location marker
-        plt.scatter(num_bins/2-0.5, num_bins-0.75, s=20, c='white', marker='v')
-        plt.text(num_bins+0.15, num_bins+1.5, 'Apoptosis location \nshown by inverted \nwhite triangle')
+        if include_apop_bin == True:
+            plt.scatter(num_bins/2-0.5, num_bins-0.75, s=20, c='white', marker='v')
+            plt.text(num_bins+0.15, num_bins+1.5, 'Apoptosis location \nshown by inverted \nwhite triangle')
+        else:
+            plt.scatter(num_bins/2-0.5, num_bins-2-0.75, s=20, c='white', marker='v')
+            plt.text(num_bins+0.15, num_bins+1.5-2, 'Apoptosis location \nshown by inverted \nwhite triangle')
 
         fn = os.path.join(save_path,title+'.pdf')
 
@@ -119,6 +131,128 @@ def auto_plot_cumulative(input_2d_hist, input_type, N, num_bins, radius, t_range
             print("Plot saved at ", fn)
             return plt.imshow(final_hist)
 
+def auto_plot_cumulative(input_2d_hist, input_type, N, num_bins, radius, t_range, focal_cell, focal_event, subject_cell, subject_event, save_parent_dir, cbar_lim, include_apop_bin, SI):
+
+        xlocs, xlabels, ylocs, ylabels = kymo_labels(num_bins, 1, radius, t_range, SI)
+
+        ## formatting cell and event names
+        focal_event_name = 'apoptoses' if 'apop' in focal_event.lower() else 'divisions' #focal_event == 'APOPTOSIS' or 'apop' else 'divisions'
+        focal_cell_name = 'wild-type' if 'wt' in focal_cell.lower() else 'Scribble'
+        subj_event_name = 'apoptoses' if 'apop' in subject_event.lower() else 'divisions'
+        subj_cell_name = 'wild-type' if 'wt' in subject_cell.lower() else 'Scribble'
+
+        if focal_event == 'control':
+            focal_event_name = 'random time points'
+
+        title = 'Spatiotemporal dist. of probability of {} {} \n around {} {} (N={})'.format(subj_cell_name, subj_event_name, focal_cell_name, focal_event_name, N)
+
+        ## output save path formatting
+        save_dir_name = '{}_{}_{}_{}'.format(focal_cell.lower(), focal_event.lower()[0:3] if focal_event == 'DIVISION' else focal_event.lower()[0:4], subject_cell.lower(), subject_event.lower()[0:3] if subject_event == 'DIVISION' else subject_event.lower()[0:4])
+        save_dir = '{}.{}.{}/{}'.format(radius,t_range,num_bins,save_dir_name)
+        save_path = os.path.join(save_parent_dir,save_dir)
+        Path(save_path).mkdir(parents=True, exist_ok=True)
+
+        ## title formatting
+        if input_type == 'N_cells':
+            title = 'Spatiotemporal dist. of {} cells \n around {} {} (N={})'.format(subj_cell_name,focal_cell_name, focal_event_name, N)
+            cb_label = 'Number of {} cell apperances'.format(subj_cell_name)
+        if input_type == 'N_events':
+            title = 'Spatiotemporal dist. of {} {} \n around {} {} (N={})'.format(subj_cell_name, subj_event_name, focal_cell_name, focal_event_name, N)
+            cb_label = 'Number of {} {}'.format(subj_cell_name, subj_event_name)
+        if input_type == 'P_events':
+            title = 'Spatiotemporal dist. of probability of {} {} \n around {} {} (N={})'.format(subj_cell_name, subj_event_name, focal_cell_name, focal_event_name, N)
+            cb_label = 'Probability of {} {}'.format(subj_cell_name, subj_event_name)
+
+        ## label unit formatting
+        if SI == True:
+            time_unit = '(Hours)'
+            distance_unit = '(Micrometers)'
+        else:
+            time_unit = '(Frames)'
+            distance_unit = '(Pixels)'
+
+        ## plotting
+        font = {'fontname':'Liberation Mono'}
+        plt.xticks(xlocs, xlabels, rotation = 'vertical', **font)
+        plt.yticks(ylocs, ylabels, **font)
+        plt.xlabel("Time since apoptosis "+ time_unit, **font)
+        plt.ylabel("Distance from apoptosis "+ distance_unit, **font)
+        plt.title(title+'\n', fontweight='bold', **font)
+
+        ## if include_apop_bin is true then the spatial bin containing the apoptotic cell (ie the central spatial bin of the radial scan) will be show in the graph, if false then it is cropped which ends up with a plot showing only the relevant local env not the site of apop (better imo)
+        if include_apop_bin == True:
+            final_hist = np.flipud(input_2d_hist)  ## flip for desired graph orientation
+        else:
+            final_hist = np.flipud(input_2d_hist[1:-1,:])
+
+        plt.imshow(final_hist)
+
+        # if cbar_lim == '':
+        #     plt.colorbar(label = cb_label, **font)
+        # else:
+        #     plt.clim(vmin=cbar_lim[0], vmax=cbar_lim[1])
+        #     plt.colorbar(label = cb_label, **font)
+
+
+
+
+        ## apop location marker
+        if include_apop_bin == True:
+            plt.scatter(num_bins/2-0.5, num_bins-0.75, s=20, c='white', marker='v')
+            plt.text(num_bins+0.15, num_bins+1.5, 'Apoptosis location \nshown by inverted \nwhite triangle', **font)
+        else:
+            plt.scatter(num_bins/2-0.5, num_bins-2-0.75, s=20, c='white', marker='v')
+            plt.text(num_bins+0.15, num_bins+1.5-2, 'Apoptosis location \nshown by inverted \nwhite triangle', **font)
+
+
+        # ### when manually editing the typeface of the colorbar
+        # if cbar_lim == '':
+        #     if include_apop_bin == False:
+        #         plt.clim(vmin=np.min(P_events[1:-1,:]), vmax=np.max(P_events[1:-1,:]))
+        #     else:
+        #         plt.clim(vmin=np.min(P_events), vmax=np.max(P_events))
+        #     plt.colorbar(label = cb_label)
+        # else:
+        #     plt.clim(vmin=cbar_lim[0], vmax=cbar_lim[1])
+        #     plt.colorbar(label = cb_label)
+
+        ## colorbar
+        if cbar_lim == '':
+            if include_apop_bin == False:
+                plt.clim(vmin=np.min(input_2d_hist[1:-1,:]), vmax=np.max(input_2d_hist[1:-1,:]))
+            else:
+                plt.clim(vmin=np.min(input_2d_hist), vmax=np.max(input_2d_hist))
+            cb = plt.colorbar(label = cb_label)
+            ax = cb.ax
+            text = ax.yaxis.label
+            font = matplotlib.font_manager.FontProperties(family='Liberation Mono')
+            text.set_font_properties(font)
+            ax.set_yticklabels(np.round(ax.get_yticks(),5),**{'fontname':'Liberation Mono'}) ### cropped to 5dp
+        else:
+            plt.clim(vmin=cbar_lim[0], vmax=cbar_lim[1])
+            cb = plt.colorbar(label = cb_label)
+            ax = cb.ax
+            text = ax.yaxis.label
+            font = matplotlib.font_manager.FontProperties(family='Liberation Mono')
+            text.set_font_properties(font)
+            ax.set_yticklabels(np.round(ax.get_yticks(),5),**{'fontname':'Liberation Mono'})
+
+        ## filename
+        fn = os.path.join(save_path,title+'.pdf')
+
+        ## failsafe overwriting block
+        if os.path.exists(fn):
+            print("Filename", fn, "already exists, saving as updated copy")
+            fn = fn.replace('.pdf', ' (updated {}).pdf'.format(time.strftime("%Y%m%d-%H%M%S")))
+
+
+        ## save out?
+        if save_parent_dir == '':
+            return plt.imshow(final_hist)
+        else:
+            plt.savefig(fn, dpi = 300, bbox_inches = 'tight')
+            print("Plot saved at ", fn)
+            return plt.imshow(final_hist)
 
 def plot_cumulative(input_2d_hist, num_bins, radius, t_range, title, label, cb_label, save_path, SI):
 
@@ -253,9 +387,34 @@ def plot_P_events(input_2d_hist, event, subject_cells, target_cell, focal_time, 
 
     return plt.imshow(final_hist)
 
-
-### labels in SI
 def kymo_labels(num_bins, label_freq, radius, t_range, SI):
+    label_freq = 1
+    radial_bin = radius / num_bins
+    temporal_bin = t_range / num_bins
+
+    if SI == True:
+        time_scale_factor = 4/60 ## each frame is 4 minutes
+        distance_scale_factor = 1/3 ## each pixel is 0.3recur micrometers
+    else:
+        time_scale_factor, distance_scale_factor = 1,1
+
+    ### generate labels for axis micrometers/hours
+    xlocs = np.arange(-0.5, num_bins, label_freq) ## -0.5 to start at far left border of first bin
+    xlabels = []
+    for m in np.arange(int(-num_bins/2), int(num_bins/2)+1,label_freq):
+        xlabels.append(str(int(((temporal_bin)*m)*time_scale_factor)))# + "," + str(int(((temporal_bin)*m+temporal_bin)*time_scale_factor)))
+
+    ylocs = np.arange(-0.5, num_bins, label_freq) ## -0.5 to start at far top border of first bin
+    ylabels = []
+    for m in np.arange(num_bins, 0-1, -label_freq):
+        ylabels.append(str(int(((radial_bin)*m)*distance_scale_factor)))# + "," + str(int(((radial_bin)*(m-1)*distance_scale_factor))))
+
+    return xlocs, xlabels, ylocs, ylabels
+
+def old_kymo_labels(num_bins, label_freq, radius, t_range, SI):
+    """
+    This plots the labels in the middle of each bin whereas the new one plots labels on the edges of each bin
+    """
     label_freq =1
     radial_bin = radius / num_bins
     temporal_bin = t_range / num_bins
